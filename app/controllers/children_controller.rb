2995f3cb6f682c4203bf40940ea02a750b7525c4
lambog2015@gmail.com <lambog2015@gmail.com>
2023-06-06 17:03:50 +0200

    maj route controller view et show

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,5 +1,38 @@
 class ChildrenController < ApplicationController
+
+
   def index
-    @children = Children.all
+    @user = current_user
+    @children = @user.children
   end
+
+  def create
+    @child = Child.new(child_params)
+    @child.save!
+    redirect_to children_path
+  end
+
+
+  def new
+    @child = Child.new
+  end
+
+
+  def show
+    @child = Child.find(params[:id])
+  end
+
+
+
+
+
+
+
+
+  private
+
+  def child_params
+    params.require(:child).permit(:first_name, :age)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  authenticated :user do
+    root 'calendars#index', as: :authenticated_root
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  resources :children, only: [:new, :create, :index, :show]
-  resources :games, only: [:index, :show]
+  resources :children, only: %i[new create index show] do
+  resources :games, only: %i[index]
+  resources :child_games, only: [:create]
+    end
+    resources :child_games, only: [:show]
 end
