032be59d9e449c6262b273fabe967423ca49e31e
Alban de Morel <albandemorel@hotmail.fr>
2023-06-06 15:35:11 +0200

    added games index

--- app/controllers/child_games_controller.rb
+++ app/controllers/child_games_controller.rb
@@ -1,6 +1,7 @@
 class ChildGamesController < ApplicationController
   def create
     @child_game = ChildGame.new(:child_id, :game_id)
+    @child_game.save!
     redirect_to child_game_path(@child_game)
   end
 
