cbcf884c5eb643620d4bef089e237f33343b509c
Alban de Morel <albandemorel@hotmail.fr>
2023-06-06 18:06:44 +0200

    routesencours

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
     root 'children#index', as: :authenticated_root
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   resources :children, only: %i[new create index show] do
     resources :games, only: %i[index]
     resources :child_games, only: [:create]
-   end
-   resources :child_games, only: [:show]
+  end
+  resources :child_games, only: [:show]
 end
