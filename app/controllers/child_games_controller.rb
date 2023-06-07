bf584a9fdcb9082e4dd2f880d543d55cceed4ec2
Alban de Morel <albandemorel@hotmail.fr>
2023-06-07 12:28:19 +0200

    routesfinished

--- app/controllers/child_games_controller.rb
+++ app/controllers/child_games_controller.rb
@@ -1,6 +1,8 @@
 class ChildGamesController < ApplicationController
   def create
-    @child_game = ChildGame.new(:child_id, :game_id)
+    @child = Child.find(params[:child_id])
+    @game = Game.find(params[:game_id])
+    @child_game = ChildGame.new(child: @child, game: @game)
     @child_game.save!
     redirect_to child_game_path(@child_game)
   end
@@ -8,5 +10,4 @@ class ChildGamesController < ApplicationController
   def show
     @child_game = ChildGame.find(params[:id])
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   authenticated :user do
-    root 'calendars#index', as: :authenticated_root
+    root 'children#index', as: :authenticated_root
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   resources :children, only: %i[new create index show] do
-    resources :games, only: %i[index]
-    resources :child_games, only: [:create]
+    resources :games, only: %i[index] do
+      resources :child_games, only: [:create]
+    end
   end
   resources :child_games, only: [:show]
-
 end
