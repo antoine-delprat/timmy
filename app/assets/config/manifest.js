6ca7a64cf71f15944ce333a0b24cadb7f03d2fc2
Antoine Delprat <antoine.delprat.pro@gmail.com>
2023-06-12 13:17:58 +0200

    first animations

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
 //= link manifest.json
+//= link won.mp3
+//= link victory.mp3
+//= link fail.mp3
+//= link reveal.mp3
--- app/javascript/controllers/memory_controller.js
+++ app/javascript/controllers/memory_controller.js
@@ -18,15 +18,26 @@ export default class extends Controller {
             // puis on vérifie si le jeu est fini, càd si toutes les cartes ont la classe 'card-won>'
           // si elles n'ont pas les mêmes, on attend 2 secondes avant de leur retirer la class 'card-revealed'
 
-      event.target.classList.add("card-revealed")
 
+    event.target.classList.add("card-revealed")
+
+
+    if (document.querySelectorAll(".card.card-revealed").length % 2 !== 0) {
+    var audioreveal = document.getElementById("sound-reveal");
+    audioreveal.play();
+    }
     if (document.querySelectorAll(".card.card-revealed").length % 2 == 0) {
       if (document.querySelectorAll(".card.card-revealed")[0].classList.value === document.querySelectorAll(".card.card-revealed")[1].classList.value) {
+        var audiowon = document.getElementById("sound-won");
+        audiowon.play();
+
         setTimeout(() => {
         document.querySelectorAll(".card.card-revealed")[0].classList.replace("card-revealed", "card-won")
         document.querySelectorAll(".card.card-revealed")[0].classList.replace("card-revealed", "card-won")
 
         if (document.querySelectorAll(".card.card-won").length == 16) {
+          var audiovictory = document.getElementById("sound-victory");
+          audiovictory.play();
           document.getElementById("modal-bloc").innerHTML = "<div id=\"modal-win\"><h1>Bravo !</h1><div class=\"d-flex justify-content-center mt-5 mb-5\"><div><div class=\"card one card-won\"></div></div><div class=\"d-grid\"><div class=\"d-flex\"><div class=\"mt-3\"><i class=\"fa-solid fa-star\"></i></div><div><i class=\"fa-solid fa-star\"></i></div><div class=\"mt-3\"><i class=\"fa-solid fa-star\"></i></div></div><h3 class=\"align-self-end\">+ 10 points</h3></div></div><div class=\"d-flex justify-content-center mt-5 mb-5\"><a href=\"/children/" + this.childValue + "/games\"><i class=\"fa-solid fa-house\"></i></a><a data-turbo-method=\"post\" href=\"/children/" + this.childValue + "/games/" + this.gameValue + "/child_games\"><i class=\"fa-solid fa-rotate-right\"></i></a></div></div>"
           document.getElementById("cardboard").style.zIndex = "-3"
         }
@@ -34,9 +45,13 @@ export default class extends Controller {
 
       }
       else {
+        var audiofail = document.getElementById("sound-fail");
+        audiofail.play();
+        document.querySelectorAll(".card.card-revealed")[0].classList.add("shake")
+        document.querySelectorAll(".card.card-revealed")[1].classList.add("shake")
         setTimeout(() => {
-          document.querySelectorAll(".card.card-revealed")[0].classList.remove("card-revealed")
-          document.querySelectorAll(".card.card-revealed")[0].classList.remove("card-revealed")
+          document.querySelectorAll(".card.card-revealed")[0].classList.remove("card-revealed", "shake")
+          document.querySelectorAll(".card.card-revealed")[0].classList.remove("card-revealed", "shake")
         }, 1300);
       }
     }
