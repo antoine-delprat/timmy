74bf211649df5a062dd5ba84025c8268a9f1e75d
matthieu boussicault <boussicaultmatthieu@gmail.com>
2023-06-13 10:02:50 +0200

    profils 1

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,6 +12,3 @@ application.register("memory", MemoryController)
 
 import MenuController from "./menu_controller"
 application.register("menu", MenuController)
-
-import TooltipController from "./tooltip_controller"
-application.register("tooltip", TooltipController)
