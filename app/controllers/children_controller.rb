d3e7c036864e98a5d1ca471f5f8acd0b530fef2c
Antoine Delprat <antoine.delprat.pro@gmail.com>
2023-06-15 11:52:28 +0200

    alarm on click, condition for end different from nil

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -29,8 +29,10 @@ class ChildrenController < ApplicationController
     # @games_names = ["Mémoire d'éléphant", ]
 
     @child_game.each do |child_game|
-      @total_score += child_game.score
-      @screen_time += ((child_game.end_date - child_game.start_date) / 60).to_i
+      unless child_game.end_date.nil?
+        @total_score += child_game.score
+        @screen_time += ((child_game.end_date - child_game.start_date) / 60).to_i
+      end
     end
 
     @week_time.each do |week_time|
@@ -76,42 +78,44 @@ class ChildrenController < ApplicationController
     @time_game9 = 0
 
       @child_game.each do |game|
-        if game.game.name == "Mémoire d'éléphant"
-          @score_game1 += game.score.to_i
-          @name_game1 = game.game.name
-          @time_game1 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Génie des maths"
-          @score_game2 += game.score.to_i
-          @name_game2 = game.game.name
-          @time_game2 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Animaux en puzzle"
-          @score_game3 += game.score.to_i
-          @name_game3 = game.game.name
-          @time_game3 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Sons en folie"
-          @score_game4 += game.score.to_i
-          @name_game4 = game.game.name
-          @time_game4 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Chasse aux papillons"
-          @score_game5 += game.score.to_i
-          @name_game5 = game.game.name
-          @time_game5 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Rebus"
-          @score_game6 += game.score.to_i
-          @name_game6 = game.game.name
-          @time_game6 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Couleurs partout"
-          @score_game7 += game.score.to_i
-          @name_game7 = game.game.name
-          @time_game7 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Jeu de billes"
-          @score_game8 += game.score.to_i
-          @name_game8 = game.game.name
-          @time_game8 += ((game.end_date - game.start_date) / 60).to_i
-        elsif game.game.name == "Trouve les lettres"
-          @score_game9 += game.score.to_i
-          @name_game9 = game.game.name
-          @time_game9 += ((game.end_date - game.start_date) / 60).to_i
+        unless game.end_date.nil?
+          if game.game.name == "Mémoire d'éléphant"
+            @score_game1 += game.score.to_i
+            @name_game1 = game.game.name
+            @time_game1 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Génie des maths"
+            @score_game2 += game.score.to_i
+            @name_game2 = game.game.name
+            @time_game2 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Animaux en puzzle"
+            @score_game3 += game.score.to_i
+            @name_game3 = game.game.name
+            @time_game3 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Sons en folie"
+            @score_game4 += game.score.to_i
+            @name_game4 = game.game.name
+            @time_game4 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Chasse aux papillons"
+            @score_game5 += game.score.to_i
+            @name_game5 = game.game.name
+            @time_game5 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Rebus"
+            @score_game6 += game.score.to_i
+            @name_game6 = game.game.name
+            @time_game6 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Couleurs partout"
+            @score_game7 += game.score.to_i
+            @name_game7 = game.game.name
+            @time_game7 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Jeu de billes"
+            @score_game8 += game.score.to_i
+            @name_game8 = game.game.name
+            @time_game8 += ((game.end_date - game.start_date) / 60).to_i
+          elsif game.game.name == "Trouve les lettres"
+            @score_game9 += game.score.to_i
+            @name_game9 = game.game.name
+            @time_game9 += ((game.end_date - game.start_date) / 60).to_i
+          end
         end
       end
   end
